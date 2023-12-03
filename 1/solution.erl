588d1175f692cbfa9532b87d1a21af9edce49289
Adam Olech <me@aole.ch>
2023-12-03 23:12:28 +0100

    1: add a naive solution for the second part

--- 1/solution.erl
+++ 1/solution.erl
@@ -1,5 +1,31 @@
 -module(solution).
--export([cal_value_sum_from_file/1]).
+-export([cal_value_sum_from_file/1, cal_value/1, replace_words/1]).
+
+numbers() ->
+    [
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    ].
+
+replace_words(Line) ->
+	replace_words(Line, numbers()).
+
+replace_words(Line, []) ->
+	lists:flatten(Line);
+
+replace_words(Line, RemainingNumbers) ->
+	[Pair|Tail] = RemainingNumbers,
+	{SearchPattern, Replacement} = Pair,
+	replace_words(
+	  string:replace(Line, SearchPattern, integer_to_list(Replacement), all), Tail
+	 ).
 
 cal_value(List) ->
 	first_int(List) * 10 + first_int(string:reverse(List)).
@@ -15,7 +41,7 @@ first_int(List) ->
 cal_value_sum(Device, Acc) ->
 	case io:get_line(Device, "") of
 		eof -> Acc;
-		Line -> cal_value_sum(Device, Acc+cal_value(Line))
+		Line -> cal_value_sum(Device, Acc+cal_value(replace_words(Line)))
 	end.
 
 cal_value_sum_from_file(Path) ->
