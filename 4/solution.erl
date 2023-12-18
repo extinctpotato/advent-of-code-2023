067c048b40c5d796f27f3d76fcbf5e23f21c076d
Adam Olech <me@aole.ch>
2023-12-18 02:05:32 +0100

    4: some initial doodles

--- 4/solution.erl
+++ 4/solution.erl
@@ -2,4 +2,17 @@
 -compile(export_all).
 -include_lib("eunit/include/eunit.hrl").
 
+whitespace_split(String) ->
+	{ok, R} = re:compile("\\s+"),
+	re:split(String, R, [{return,list}]).
 
+parse_numbers(String) ->
+	lists:map(
+	  fun(E) -> element(1, string:to_integer(E)) end,
+	  whitespace_split(String)
+	 ).
+
+parse_line(Line) ->
+	[CardL|Numbers] = Line,
+	Card = list_to_integer(lists:last(string:split(CardL, "Card "))),
+	{Card}.
