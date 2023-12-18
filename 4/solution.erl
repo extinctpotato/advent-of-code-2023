f4322a1dfa34361a3a8bed7a1a07c847b234e9a7
Adam Olech <me@aole.ch>
2023-12-18 22:35:18 +0100

    4: fix parser and add a test

--- 4/solution.erl
+++ 4/solution.erl
@@ -13,6 +13,20 @@ parse_numbers(String) ->
 	 ).
 
 parse_line(Line) ->
-	[CardL|Numbers] = Line,
+	[CardL,Numbers] = string:split(Line, ": "),
 	Card = list_to_integer(lists:last(string:split(CardL, "Card "))),
-	{Card}.
+	[Winning, Ours] = lists:map(
+			    fun(E) -> parse_numbers(E) end,
+			    string:split(Numbers, " | ")
+			   ),
+	{Card, Winning, Ours}.
+
+%%% Tests
+
+parser_test_() ->
+	Line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+	{CardNo, Winning, Ours} = solution:parse_line(Line),
+	[?_assert(CardNo =:= 1),
+	 ?_assert(Winning =:= [41,48,83,86,17]),
+	 ?_assert(Ours =:= [83,86,6,31,17,9,48,53])
+	].
