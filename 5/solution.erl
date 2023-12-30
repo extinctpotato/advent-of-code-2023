29815c7ed893a0fafa41fbe7f6ebbba5a53ef6b3
Adam Olech <me@aole.ch>
2023-12-30 01:34:14 +0100

    5: implement parse_numbers

--- 5/solution.erl
+++ 5/solution.erl
@@ -2,6 +2,15 @@
 -compile(export_all).
 -include_lib("eunit/include/eunit.hrl").
 
+parse_numbers(Line) ->
+	lists:map(
+	  fun(E) -> case string:to_integer(E) of
+			    {Int, []} -> Int
+		    end
+	  end,
+	  string:split(Line, " ", all)
+	 ).
+
 process_lines(Device, Acc) ->
 	case io:get_line(Device, "") of
 		eof -> Acc;
@@ -13,3 +22,16 @@ process_lines(Device, Acc) ->
 process_file(Path) ->
 	{_, Device} = file:open(Path, [read]),
 	process_lines(Device, 0).
+
+%%% Tests
+
+parse_numbers_test_() ->
+	[?_assert(parse_numbers(L) =:= V) 
+	 || {L,V} <- [
+		      {"50 98 2", [50, 98,2]},
+		      {"52 50 48", [52, 50, 48]},
+		      {"0 15 37", [0, 15, 37]},
+		      {"0 69 1", [0, 69, 1]},
+		      {"1 0 69", [1, 0, 69]}
+		     ]
+	].
