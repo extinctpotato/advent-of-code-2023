cb9e47beb66f63b61f7dd5e6437381e257a9d979
Adam Olech <me@aole.ch>
2023-12-30 17:46:47 +0100

    5: implement some more parsing logic

--- 5/solution.erl
+++ 5/solution.erl
@@ -11,6 +11,24 @@ parse_numbers(Line) ->
 	  string:split(Line, " ", all)
 	 ).
 
+parse_seeds(Line) ->
+	{ok, R} = re:compile("seeds:\s((?:\s*[0-9]+)+)"),
+	case re:split(Line, R, [{return, list}]) of
+		[[], Numbers, []] -> parse_numbers(Numbers)
+	end.
+
+parse_map_header(Line) ->
+	{ok, R} = re:compile("([a-z]+)\-to\-([a-z]+)\smap:"),
+	case re:run(Line, R, [{capture, all, list}]) of
+		{match, [Line, Source, Destination]} -> {Source, Destination}
+	end.
+
+parse_line(Line, {[],[]}) -> {parse_seeds(Line), []}; % parse first line
+parse_line("", {S,[]}) -> {S, []};                    % parse second line
+
+parse_line(Line, {_S, Maps}) ->
+	Line. % TODO: implement
+
 process_lines(Device, Acc) ->
 	case io:get_line(Device, "") of
 		eof -> Acc;
