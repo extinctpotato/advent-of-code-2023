77c39d16af694602bbb60c051469d767b0f13757
Adam Olech <me@aole.ch>
2024-03-04 01:27:15 +0100

    6: implement text parsing functions

--- 6/solution.erl
+++ 6/solution.erl
@@ -24,3 +24,25 @@ margin_of_error([], Acc) -> Acc;
 margin_of_error([Race|Races], Acc) -> margin_of_error(Races, Acc*ways_to_win(Race)).
 
 margin_of_error(Races) -> margin_of_error(Races, 1).
+
+%%% Text-parsing functions
+
+leading_number_terminal(R, Digits) ->
+	case string:to_integer(string:reverse(Digits)) of
+		{Int, []} -> {Int, R}
+	end.
+leading_number([32|R], []) -> leading_number(R, []);
+leading_number([32|R], Digits) -> leading_number_terminal(R, Digits);
+leading_number([], Digits) -> leading_number_terminal([], Digits);
+leading_number([Digit|R], Digits) -> leading_number(R, [Digit|Digits]).
+
+leading_number_all(Line) -> leading_number_all([], Line).
+leading_number_all(Numbers, []) -> lists:reverse(Numbers);
+leading_number_all(Numbers, Line) ->
+	{Number, Line2} = leading_number(Line, []),
+	leading_number_all([Number|Numbers], Line2).
+
+parse_line([Title, Numbers]) ->
+	{list_to_atom(string:lowercase(Title)), leading_number_all(Numbers)};
+parse_line(Line) ->
+	parse_line(string:split(Line, ":")).
